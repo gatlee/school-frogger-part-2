58350536083d9ea7204788c451f808013d26a8e7
Your Name <gatleekaw@gmail.com>
2018-09-01 18:43:57 +1000

    Draw grass, water and player tiles correctly

--- src/World.java
+++ src/World.java
@@ -4,9 +4,17 @@ import org.newdawn.slick.SlickException;
 
 public class World {
     private Sprite player;
+    private SpritesTiled grass;
+    private SpritesTiled water;
 
 	public World() throws SlickException {
-		player = new Player("assets/frog.png", 0, 0);
+		player = new Player("assets/frog.png", 512, 720);
+		grass = new SpritesTiled("assets/grass.png",
+				24, 384, 22,
+				6 );
+		water = new SpritesTiled("assets/water.png",
+				24, 96, 22,
+				6 );
 	}
 	
 	public void update(Input input, int delta) {
@@ -16,6 +24,8 @@ public class World {
 	
 	public void render(Graphics g) {
 		// Draw all of the sprites in the game
+		grass.render();
+		water.render();
 		player.render();
 	}
 }
