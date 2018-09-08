ff9cce7815ab87d00225fb49f9c4814ea2bdcbe5
Your Name <gatleekaw@gmail.com>
2018-09-08 15:39:11 +1000

    Made Buses into a SpriteCollection so that collision could be used in the future

--- src/BusRow.java
+++ src/BusRow.java
@@ -5,33 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BusRow {
-    private List<Bus> buses;
-    private String direction;
-    private float yPosition;
-    private int offset;
-    private float tilesApart;
 
     private final static int SCREEN_WIDTH = App.SCREEN_WIDTH;
     private final static int SCREEN_HEIGHT = App.SCREEN_HEIGHT;
     private final static int TILE_SIZE = App.TILE_SIZE;
 
-    public BusRow(String direction, float yPosition, int offset,
-                  float tilesApart) throws SlickException {
-        this.direction = direction;
-        this.yPosition = yPosition;
-        this.offset = offset;
-        this.tilesApart = tilesApart;
-        this.generateBusesArray();
+    public BusRow() {
 
 
     }
 
-    private void generateBusesArray() throws SlickException {
+    public List<Bus> generateBusesArray(String direction, float yPosition,
+                                        int offset, float tilesApart)
+            throws SlickException {
         float startX;
         int incrementAmount;
 
-        float distanceBetweenBuses = this.tilesApart * App.TILE_SIZE;
-        buses = new ArrayList<Bus>();
+        float distanceBetweenBuses = tilesApart * App.TILE_SIZE;
+        List<Bus> buses = new ArrayList<Bus>();
 
         /*Set first bus location and loop increment direction based on direction*/
         switch (direction) {
@@ -49,42 +40,30 @@ public class BusRow {
 
 
         /*Populate array with buses at specified location with correct tile spacing*/
-        while (this.isInBounds(startX)) {
-            createBusAtXPosition(startX);
+        while (isInBounds(startX)) {
+            buses.add(createBusAtXPosition(startX, yPosition, direction));
             startX += (incrementAmount * distanceBetweenBuses);
         }
+        return buses;
 
     }
 
     /* Helper function to create a bus at x location
      * All other attributes assumed based on parameters given */
-    private void createBusAtXPosition(float x) throws SlickException {
-        Bus newBus = new Bus(x, this.yPosition, direction);
-        if (direction == "right") {
+    private Bus createBusAtXPosition(float x, float yPosition, String direction) throws SlickException {
+        Bus newBus = new Bus(x, yPosition, direction);
+        if (direction.equals("right")) {
             newBus.setInitialXPosition(0 - newBus.getImageWidth() / 2f);
-        } else if (direction == "left") {
+        } else if (direction.equals("left")) {
             newBus.setInitialXPosition(SCREEN_WIDTH + newBus.getImageWidth() / 2f);
         }
 
-        buses.add(newBus);
+        return newBus;
     }
 
     private boolean isInBounds(float x) {
         return (x >= 0 && x <= SCREEN_WIDTH);
     }
 
-    //Slick update function
-    public void update(Input input, int delta) {
-        for (Bus bus : this.buses) {
-            bus.update(input, delta);
-        }
-    }
-
-    //Slick render function
-    public void render() {
-        for (Bus bus : this.buses) {
-            bus.render();
-        }
-    }
 }
 
