cbdce8fe381c79e75239a093b5b5ba28099df352
Gatlee Kaw <gatlee.kaw@gmail.com>
2018-10-05 21:44:36 +1000

    Rudimentary Parsing of lvl file to generate sprites

--- src/EntityGenerator.java
+++ src/EntityGenerator.java
@@ -2,6 +2,7 @@ import org.newdawn.slick.SlickException;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,6 +10,10 @@ public final class EntityGenerator {
     private final static int SCREEN_WIDTH = App.SCREEN_WIDTH;
     private final static int SCREEN_HEIGHT = App.SCREEN_HEIGHT;
     private final static int TILE_SIZE = App.TILE_SIZE;
+    private final static int LVL_SPRITE_TYPE_INDEX = 0;
+    private final static int LVL_X_POS_INDEX = 1;
+    private final static int LVL_Y_POS_INDEX = 2;
+    private final static int LVL_DIRECTION_INDEX = 3;
 
     //Generate array of all buses and returns it
     public static List<Sprite> getSpriteListFromFile(String fileLocation) throws SlickException {
@@ -30,8 +35,48 @@ public final class EntityGenerator {
 
     }
 
+    private static String parseDirection(String direction) throws SlickException {
+        if (direction.equals("true")) {
+            return "right";
+
+        } else if (direction.equals("false")) {
+            return "left";
+
+        } else {
+            throw new SlickException("Parsing Error. Unexpected direction");
+        }
+
+    }
     private static Sprite generateSpriteFromString(String spriteSpecification) throws SlickException {
-        return new Bus(10, 10, "right");
+        String[] spriteParameters = spriteSpecification.split(",");
+        int x = Integer.parseInt(spriteParameters[LVL_X_POS_INDEX]);
+        int y = Integer.parseInt(spriteParameters[LVL_Y_POS_INDEX]);
+        String direction = null;
+
+        if (spriteParameters.length == 4) {
+            direction = parseDirection(spriteParameters[LVL_DIRECTION_INDEX]);
+        }
+
+        switch (spriteParameters[LVL_SPRITE_TYPE_INDEX]) {
+            case "log":
+                break;
+            case "longLog":
+                break;
+            case "water":
+                return new Water(x, y);
+            case "grass":
+                return new Grass(x, y);
+            case "tree":
+                break;
+            case "bus":
+                return new Bus(x, y, direction);
+            case "bulldozer":
+                break;
+
+        }
+
+        //Default case for now. Should probably do something else once other classes implemented
+        return new Grass(5000, 5000);
 
 
     }
